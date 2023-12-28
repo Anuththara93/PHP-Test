ca644ee2f34588c40339e81c5fa137be728d2b74
Anuththara93 <2021cs009@stu.ucsc.cmb.ac.lk>
2023-12-28 21:26:33 +0530

    PHP tutorial

---
+++ about.php
@@ -0,0 +1,21 @@
+<?php
+    include("header.html");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial">
+        <title>Document</title>
+    </head>
+    <body>
+        This is the About Page<br>
+        Stuff about About Page can go here<br>
+    </body>
+</html>
+
+<?php
+    include("footer.html");
+?>
---
+++ locations.php
@@ -0,0 +1,21 @@
+<?php
+    include("header.html");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial">
+        <title>Document</title>
+    </head>
+    <body>
+        This is the Location Page<br>
+        Stuff about Location Page can go here<br>
+    </body>
+</html>
+
+<?php
+    include("footer.html");
+?>
